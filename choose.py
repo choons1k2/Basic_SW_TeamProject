85942b484edd760f5a977b6a829d847856e6e4cf
choonsik <108234111+choons1k2@users.noreply.github.com>
2022-11-20 17:53:10 +0900

    Add files via upload
    2022.11.20

--- choose.py
+++ choose.py
@@ -1,7 +1,7 @@
 class throwAgain:
     def chooseDice(self):
         throwAgain = input("몇번째 주사위를 다시 던질까요? (띄어쓰기로 구분): ")
-        throwAgain = throwAgain.split()
+        throwAgain = list(map(int, throwAgain.split()))
 
         return throwAgain
         
